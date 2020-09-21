6a6f17d59ffaa8e8eaa55a9143ff416ffee65eda
minnaho <minnaho95@gmail.com>
2020-09-21 10:17:42 -0700

    created python version of rainfall-runoff model, working to create river file

---
+++ river_data/updated_2013_2017/export_rivers_excel.py
@@ -0,0 +1,14 @@
+# export river flows to excel
+import pandas as pd
+import numpy as np
+import glob as glob
+
+usgspath = '/data/project1/minnaho/river_data/updated_2013_2017/usgs_rivers/daily/'
+modelpath = '/data/project1/minnaho/river_data/updated_2013_2017/rational_method_rivers/runoff_model_output/'
+extrapath = '/data/project1/minnaho/river_data/updated_2013_2017'
+
+rfiles = sorted(glob.glob(rpath))
+
+
+
+
