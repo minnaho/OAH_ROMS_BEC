used ncjoin, fixed units in POTW netcdf files

potw_outfall_data/updated_2013_2017/make_netcdf/turn_daily_to_monthly_minors.py
@@ -1013,7 +1013,7 @@ sal_v = ncf.createVariable('salinity',np.float64,('time','location'))
 dfe_v = ncf.createVariable('dissolved_Fe',np.float64,('time','location'))
 
 tim_v.units = timeunit
-flo_v.units = 'mmol/m3'
+flo_v.units = 'm3/s'
 nh4_v.units = 'mmol/m3'
 no3_v.units = 'mmol/m3'
 no2_v.units = 'mmol/m3'
@@ -1122,7 +1122,7 @@ sal_v = ncf.createVariable('salinity',np.float64,('time','location'))
 dfe_v = ncf.createVariable('dissolved_Fe',np.float64,('time','location'))
 
 tim_v.units = timeunit
-flo_v.units = 'mmol/m3'
+flo_v.units = 'm3/s'
 nh4_v.units = 'mmol/m3'
 no3_v.units = 'mmol/m3'
 no2_v.units = 'mmol/m3'